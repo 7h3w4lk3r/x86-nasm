4ad0ad6d0c7877c8b6e2fc25d6ccdb574970e392
root <root@W4LK3R>
2021-05-24 00:40:22 +0430

    new programs added

--- section-2/and.asm
+++ section-2/and.asm
@@ -1,39 +1,48 @@
-; demo for and instruction
+; a demo for AND logical instruction
 
 section .data
-even_msg db 'Even Number!'
-len1 equ $ - even_msg
-odd_msg db 'Odd Number!'
-len2 equ $ - odd_msg
+odd_msg db "odd number!",0ah
+odd_len equ $ - odd_msg
+even_msg db "even number!",0ah
+even_len equ $ - even_msg
+
 
 section .text
 global _start
 
 _start:
-mov	ax, 8h		; put decimal 8 in ax
-and	ax, 1		; and ax with 1 to see if its odd or even
+mov	eax, 8h		; put 8 in eax ( 8 is even so the 'even' subroutine will be executed)
+and	eax, 1		; AND eax with 1
+jz	even		; if the result of AND is even (zero flag is set), jump to 'even' subroutine
+jnz	odd		; if the result of AND is odd (zero flag not set), jump to 'odd' subroutine
 
-jz	even		; if the zero flag was set (8 was an even number) jump to the 'even' block
-
-mov	eax, 4		; if the zero flag was not set (8 was an odd number) print the odd message
-mov	ebx, 1
-mov	ecx, odd_msg
-mov	edx, len1
-int	80h
-
-jmp	_exit		; unconditional jump to '_exit' block when the message was printed
-
-even:			; print odd message
-mov	eax, 4
-mov	ecx, odd_msg
-mov	edx, len2
-mov	ebx, 1
-int	80h
-
-_exit:			; exit code
+mov	ebx, 0		; exit gracefully
 mov	eax, 1
-mov	ebx, 0
 int	80h
+
+
+even:
+mov	eax, 4
+mov	ebx, 1
+mov	ecx, even_msg
+mov	edx, even_len
+int	80h
+ret
+
+
+odd:
+mov     eax, 4
+mov     ebx, 1
+mov     ecx, odd_msg
+mov     edx, odd_len
+int     80h
+ret
+
+
+
+
+
+
 
 
 
--- section-2/or.asm
+++ section-2/or.asm
@@ -1,24 +1,25 @@
-; a demo for OR instruction
+; a demo for OR logical instruction
 
-section .bss
-result resb 1
 
 section .text
 global _start
 
 _start:
-mov	al, 5		; put 5 in al
-mov	bl, 3		; put 3 in bl
-or	al, bl		; or al and bl (7)
-add	al, byte '0'	; convert decimal 7 to ascii '7'
-mov	[result], al	; store result in 'result'
+mov    al, 5             ; put 5 in al
+mov    bl, 3             ; put 3 in bl
+or     al, bl            ; OR al with bl, result should be 7
+add    al, byte '0'      ; convert decimal to ascii and put it in al
 
-mov	eax, 4		; print the result
-mov	ebx, 1
-mov	ecx, result
-mov	edx, 1
-int	80h
+mov    [result],  al	; move the value in al to the address of result variable (brackets are the refrence for an address)
+mov    eax, 4		; print out the result...
+mov    ebx, 1
+mov    ecx, result
+mov    edx, 1
+int    0x80
 
-mov	eax, 1
-mov	ebx, 0
-int	80h
+mov	ebx, 0		; exit the program
+mov    eax,1
+int    0x80
+
+section    .bss
+result resb 1
